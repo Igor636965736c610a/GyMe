6fdad85258ece3f02e897ca51f1f3bb8f4fec620
Igor <igormiekina@gmail.com>
2023-04-22 20:06:29 +0200

    Simple Exercise Controller

---
+++ GymAppApi/BodyRequest/SimpleExercise/PostSimpleExerciseBody.cs
@@ -0,0 +1,10 @@
+﻿using GymAppCore.Models.Entities;
+
+namespace GymAppApi.BodyRequest.SimpleExercise;
+
+public class PostSimpleExerciseBody
+{
+    public ExercisesType ExercisesType { get; set; }
+    public string? Series { get; set; }
+    public string? Description { get; set; }
+}
---
+++ GymAppApi/BodyRequest/SimpleExercise/PutSimpleExerciseBody.cs
@@ -0,0 +1,8 @@
+﻿namespace GymAppApi.BodyRequest.SimpleExercise;
+
+public class PutSimpleExerciseBody
+{
+    public string ExerciseId { get; set; }
+    public string? Description { get; set; }
+    public string? Series { get; set; } 
+}
