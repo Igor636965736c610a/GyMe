62269b5ca357bc8e2308f461eab7a963b5b390b1
Igor636965736c610a <igormiekina@gmail.com>
2023-04-28 17:49:48 +0200

    bog fix

--- GymAppApi/Routes/v1/ApiRoutes.cs
+++ GymAppApi/Routes/v1/ApiRoutes.cs
@@ -5,6 +5,7 @@ public static class ApiRoutes
     public static class Account
     {
         public const string Login = "login";
+        public const string GetAccountInformation = "get";
         public const string Register = "register";
         public const string ConfirmEmail = "confirmEmail";
         public const string UpdateUser = "update";
--- GymAppInfrastructure/Migrations/20230428135238_Initial3.Designer.cs
+++ GymAppInfrastructure/Migrations/20230428135238_Initial3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GymAppInfrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GymAppInfrastructure.Migrations
 {
     [DbContext(typeof(GymAppContext))]
-    partial class GymAppContextModelSnapshot : ModelSnapshot
+    [Migration("20230428135238_Initial3")]
+    partial class Initial3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
