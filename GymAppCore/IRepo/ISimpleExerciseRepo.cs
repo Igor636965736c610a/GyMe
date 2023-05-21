c7936549dbb6b28dcdc1699ee2a559b58a0c83f2
Igor <igormiekina@gmail.com>
2023-05-21 10:17:07 +0200

    niedokonczone

--- GymAppCore/IRepo/ISimpleExerciseRepo.cs
+++ GymAppCore/IRepo/ISimpleExerciseRepo.cs
@@ -7,8 +7,15 @@ public interface ISimpleExerciseRepo
     Task<SimpleExercise?> Get(Guid id);
     Task<List<SimpleExercise>> GetAll(Guid userId, Guid exerciseId, int page, int size);
     Task<List<SimpleExercise>> GetAll(Guid userId, Guid exerciseId);
-    Task<Dictionary<Guid, string?>> GetMaxReps(Guid userId, IEnumerable<Guid> exercisesIds);
-    Task<string?> GetMaxRep(Guid userId, Guid exercisesId);
+    Task<Dictionary<Guid, SimpleExercise?>> GetMaxRepForExercises(IEnumerable<Guid> exercisesId);
+    Task<SimpleExercise?> GetMaxRepForExercise(Guid exerciseId);
+
+    Task<Dictionary<Exercise, int>> GetScoresss(IEnumerable<Guid> exercisesId, int size,
+        Func<IEnumerable<Series>, int> calculate);
+
+    Task<Dictionary<Exercise, int>> GetScoress(IEnumerable<Guid> exercisesId, int size,
+        Func<IEnumerable<Series>, int> calculate);
+
     Task<bool> Create(SimpleExercise exercise);
     Task<bool> Update(SimpleExercise exercise);
     Task<bool> Remove(SimpleExercise exercise);
