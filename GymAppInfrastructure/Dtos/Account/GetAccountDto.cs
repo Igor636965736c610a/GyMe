c9aa956b546d504d0669dc062412185fd16e401a
Igor636965736c610a <igormiekina@gmail.com>
2023-06-16 20:22:47 +0200

    Chaaaaad

--- GymAppInfrastructure/Dtos/Account/GetAccountDto.cs
+++ GymAppInfrastructure/Dtos/Account/GetAccountDto.cs
@@ -2,6 +2,7 @@
 
 public class GetAccountDto
 {
+    public Guid Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public bool PrivateAccount { get; set; }
