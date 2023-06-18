58731374225d8c1f52d83dacbc4041a4399f444e
Igor <igormikekina@gmail.com>
2023-06-18 22:04:37 +0200

    /get

--- GymAppCore/Models/AuthenticationLoginResult.cs
+++ GymAppCore/Models/AuthenticationLoginResult.cs
@@ -3,6 +3,7 @@
 public class AuthenticationLoginResult
 {
     public string Token { get; set; }
+    public Guid UserId { get; set; }
     public bool Success { get; set; }
     public IEnumerable<string>? Errors { get; set; }
 }
