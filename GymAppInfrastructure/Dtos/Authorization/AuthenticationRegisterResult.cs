ce11d65cc26f2cd18411c3d2a95aaae4ed48a1d3
Igor636965736c610a <igormiekina@gmail.com>
2023-06-30 20:12:47 +0200

    External Login

--- GymAppInfrastructure/Dtos/Authorization/AuthenticationRegisterResult.cs
+++ GymAppInfrastructure/Dtos/Authorization/AuthenticationRegisterResult.cs
@@ -1,9 +1,10 @@
-﻿namespace GymAppCore.Models;
+﻿namespace GymAppInfrastructure.Dtos.Authorization;
 
 public class AuthenticationRegisterResult
 {
     public Guid UserId { get; set; }
     public string Token { get; set; }
     public bool Success { get; set; }
+    public IEnumerable<string> Messages { get; set; }
     public IEnumerable<string> Errors { get; set; }
 }
