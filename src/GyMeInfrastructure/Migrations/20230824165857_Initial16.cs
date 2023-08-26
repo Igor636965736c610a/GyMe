74f3090ba8952450c23ef96b466e110ed032a916
Igor <igormiekina@gmail.com>
2023-08-26 15:15:48 +0200

    MongoDb, docker, FluentValidation 1/2, FindUser BugFix, small changes

---
+++ src/GyMeInfrastructure/Migrations/20230824165857_Initial16.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymAppInfrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial16 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "ExtendedUsers",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "ExtendedUsers");
+        }
+    }
+}
